b46deab7525f640a3eff5e1571c556eb4b1870f0
c0b23113 <c0b2311324@edu.teu.ac.jp>
2024-09-24 16:38:10 +0900

    練習7:背景改善（いびつな雲解消済み）

--- pygame_image.py
+++ pygame_image.py
@@ -10,20 +10,31 @@ def main():
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg_img= pg.image.load("fig/pg_bg.jpg")
-    bg_img1 = pg.image.load("fig/pg_bg.jpg")
+    bg_img2=pg.transform.flip(bg_img,True,False)
     character_img=pg.image.load("fig/3.png")
     character_img=pg.transform.flip(character_img,True,False)
+    character_rect=character_img.get_rect()
+    character_rect.center=300,200
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
+        key_lst=pg.key.get_pressed()
+        if key_lst[pg.K_UP]:
+            character_rect.move_ip(0,-1)
+        if key_lst[pg.K_DOWN]:
+            character_rect.move_ip(0,+1)
+        if key_lst[pg.K_LEFT]:
+            character_rect.move_ip(-1,0)
+        if key_lst[pg.K_RIGHT]:
+            character_rect.move_ip(+1,0)
 
         x=-(tmr%3200)
         screen.blit(bg_img, [x, 0])
-        screen.blit(bg_img1,[x+1600,0])
+        screen.blit(bg_img2,[x+1600,0])
         screen.blit(bg_img,[x+3200,0])
-        screen.blit(bg_img1,[x+4800,0])
-        screen.blit(character_img,[300,200])
+        screen.blit(bg_img2,[x+4800,0])
+        screen.blit(character_img,character_rect)
         pg.display.update()
         tmr += 1        
         clock.tick(200)
